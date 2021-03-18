5daa3a8d2217c2aa93faf421a50a171150a3cfcb
a.halimov@taipit.ru <a.halimov@taipit.ru>
2021-03-18 14:03:43 +0300

    Просмотрел всё ещё раз перед отправкой на проверку

--- src/ru/halimov/TestTaskStringParser.java
+++ src/ru/halimov/TestTaskStringParser.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class TestTaskStringParser {
     //Результирующие данные
-    StringBuilder result;
+    private StringBuilder result;
 
     public static void main(String[] arg) {
 
